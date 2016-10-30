d738c4437c8a1cbdee1c3673ad3b536de7be6cc4
^_____^ Kayla <^_____^ Kayla>
2016-10-29 23:51:06 -0700

    Adding private event button

--- modules/calendar/client/controllers/calendar.client.controller.js
+++ modules/calendar/client/controllers/calendar.client.controller.js
@@ -123,7 +123,7 @@ Here are the dependency injections for this controller. CalendarService is the s
         start: vm.selectedDate.local(),
         end: vm.selectedDate.local(),
         className: ['coffeeBreak'],
-        stick: true
+        stick: true,
       });
 
       newEvent.$save(function(data) {
