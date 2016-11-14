8b9eeee09a27dfe804e21db105495070e5a292a4
mjohnstone14 <mjohnstone@eaglerockschool.org>
2016-11-14 12:32:43 -0800

    created controllers for both dropdowns, updated view to use controllers, alphabetized properties in css

--- modules/songmaker/client/controllers/list-songmaker.client.controller.js
+++ modules/songmaker/client/controllers/list-songmaker.client.controller.js
@@ -3,13 +3,35 @@
 
   angular
     .module('songmaker')
+    .controller('DropdownProgressionsCtrl', DropdownProgressionsCtrl)
+    .controller('DropDownKeyCtrl', DropDownKeyCtrl)
     .controller('SongmakerListController', SongmakerListController);
 
   SongmakerListController.$inject = ['SongmakerService'];
 
+  function DropdownProgressionsCtrl() {
+    var vm = this;
+    vm.title = 'Progressions';
+    vm.progressions = [
+      { name: 'Pop', progression: 'I-IV-V', numprog: [1, 4, 5], minors: [] },
+      { name: 'Pop 2', progression: 'I-V-IV', numprog: [1, 5, 4], minors: [] },
+      { name: 'Fifties', progression: 'I-vi-IV-I', numprog: [1, 6, 4, 1], minors: [6] },
+      { name: 'Rocky', progression: 'I-vi-IV-V', numprog: [1, 6, 4, 5], minors: [6] }
+    ];
+
+  }
+
+  function DropDownKeyCtrl() {
+    var vm = this;
+    vm.title = 'Keys';
+    vm.keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+  }
+
   function SongmakerListController(SongmakerService) {
     var vm = this;
-
     vm.songmaker = SongmakerService.query();
   }
+
+
 }());
