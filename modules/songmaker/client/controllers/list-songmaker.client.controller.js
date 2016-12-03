c00052f2e54837b3bebcdc71db6ac047eebef695
Elias McSeveney <emcseveney@pugetsound.edu>
2016-12-02 20:21:34 -0800

    consolidated controllers, create button now functions

--- modules/songmaker/client/controllers/list-songmaker.client.controller.js
+++ modules/songmaker/client/controllers/list-songmaker.client.controller.js
@@ -4,7 +4,6 @@
   angular
     .module('songmaker')
     .controller('DropdownProgressionsCtrl', DropdownProgressionsCtrl)
-    .controller('DropDownKeyCtrl', DropDownKeyCtrl)
     .controller('SongmakerListController', SongmakerListController);
 
   SongmakerListController.$inject = ['SongmakerService'];
@@ -12,6 +11,10 @@
   function DropdownProgressionsCtrl() {
     var vm = this;
     vm.title = 'Progressions';
+    vm.testProg = ['Select progression and key'];
+    vm.keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    vm.notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+    vm.steps = [0, 2, 4, 5, 7, 9, 11];
     vm.progressions = [
       { name: 'Pop', progression: 'I-IV-V', numprog: [1, 4, 5], minors: [] },
       { name: 'Pop 2', progression: 'I-V-IV', numprog: [1, 5, 4], minors: [] },
@@ -19,19 +22,22 @@
       { name: 'Rocky', progression: 'I-vi-IV-V', numprog: [1, 6, 4, 5], minors: [6] }
     ];
 
-    vm.setCurrent= function(val) {
-         vm.current = val;
-    }
-  }
-
-  function DropDownKeyCtrl() {
-    var vm = this;
-    vm.title = 'Keys';
-    vm.keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-
-    vm.setCurrent= function(val) {
-         vm.current = val;
-    }
+    vm.setCurrentProgression = function(val) {
+      vm.currentProgression = val;
+    };
+    vm.setCurrentKey = function(val) {
+      vm.currentKey = val;
+    };
+    vm.testCreate = function(v1, v2) {
+      var prog = v1;
+      var k = vm.notes.indexOf(v2);
+      vm.testProg = [];
+      for (var i = 0; i < prog.length; i++) {
+        var step = prog[i] - 1;
+        vm.testProg[i] = vm.notes[(k + vm.steps[step]) % 12];
+      }
+      return vm.testProg;
+    };
 
   }
 
@@ -39,20 +45,5 @@
     var vm = this;
     vm.songmaker = SongmakerService.query();
   }
-
-/*
-  function CreateButtonController(progression, key) {
-    var vm = this;
-    vm.scale = ['A','B','C','D','E','F','G'];
-
-    if(progression.length === 0) // then do random progression
-    if(key.length === 0) //do random key
-    if(key === 'A') {
-      for(int i = 0; i < progression.length; i++) {
-        progression[i] = vm.scale[progression[i]]; //the number in the progression is replaced with a letter
-      }
-    }
-
-  } */
 
 }());
